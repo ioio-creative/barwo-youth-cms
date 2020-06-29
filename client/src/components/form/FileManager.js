af7e5624b89dcecfcc6f5c5745394758993ac659
hcc1232001 <qes4d26@hotmail.com>
2020-06-29 15:30:52 +0800

    update
    fix richtextbox value update

--- client/src/components/form/FileManager.js
+++ client/src/components/form/FileManager.js
@@ -1,5 +1,15 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
+import './FileManager.css';
+
+// routes //
+// images
+// videos
+// audios
+// pdfs
+// find by name //
+// filterText
 
 const media = [
   {
@@ -14,18 +24,43 @@ const media = [
     "alt": "this is a sample image",
     "type": "image",
     "tags": ["ccc"],
-    "src": "http://placehold.it/300x200?text=Image 2",
+    "src": "http://placehold.it/400x200?text=Image 2",
   },
   {
     "name": "Image 3 (b, c)",
     "alt": "this is a sample image",
     "type": "image",
     "tags": ["ccc", "bbb"],
-    "src": "http://placehold.it/300x200?text=Image 3",
+    "src": "http://placehold.it/400x300?text=Image 3",
+  },
+  {
+    "name": "Image 4 (a, b)",
+    "alt": "this is a sample image",
+    "type": "image",
+    "tags": ["aaa", "bbb"],
+    "src": "http://placehold.it/400x225?text=Image 4",
+  },
+  {
+    "name": "Image 5 (c)",
+    "alt": "this is a sample image",
+    "type": "image",
+    "tags": ["ccc"],
+    "src": "http://placehold.it/300x200?text=Image 5",
+  },
+  {
+    "name": "Image 6 (b, c)",
+    "alt": "this is a sample image",
+    "type": "image",
+    "tags": ["ccc", "bbb"],
+    "src": "http://placehold.it/300x200?text=Image 6",
   },
 ]
 const tags = ["aaa", "bbb", "ccc"];
+
 const FileManager = () => {
+  const [showDetails, setShowDetails] = useState(false);
+  const [selectedTag, setSelectedTag] = useState([]);
+  // const [showDetails, setShowDetails] = useState(false);
   const searchQuery = document.location.search;
   const searchParams = new URLSearchParams(searchQuery);
   const CKEditorFuncNum = searchParams.get('CKEditorFuncNum');
@@ -42,27 +77,47 @@ const FileManager = () => {
     // send request to server?
     // filter locally?
     // .then( refresh )
+    setSelectedTag((prevSelectedTag) => {
+      const searchTagIdx = prevSelectedTag.indexOf(tagName);
+      if (searchTagIdx === -1) {
+        return [
+          ...prevSelectedTag,
+          tagName
+        ];
+      } else {
+        return [
+          ...prevSelectedTag.slice(0, searchTagIdx),
+          ...prevSelectedTag.slice(searchTagIdx + 1, prevSelectedTag.length)
+        ];
+      }
+    })
   }
   const selectFile = () => {
     // ???
   }
-  return <div className="fileManager">
-    <div className="tags">
+  const toggleDetails = () => {
+    setShowDetails((prevShowDetails) => !prevShowDetails);
+  }
+  return <div className={`fileManager${showDetails ? " show" : ""}`}>
+    <div className="w3-row-padding w3-section w3-stretch tags">
       {tags.map(tag => {
-        return <div className="tag" onClick={() => selectTag(tag['name'])}>
-          {tag['display']}
+        return <div className={`w3-border w3-btn${selectedTag.indexOf(tag) !== -1 ? ' w3-blue' : ''} tag`} key={tag} onClick={() => selectTag(tag)}>
+          {tag}
         </div>
       })}
     </div>
     <div className="w3-row-padding w3-section w3-stretch media">
       {media.map(medium => {
         return (
-          <div className="w3-col s4 medium"
+          <div className={`w3-col s4 medium w3-margin-bottom`}
             onClick={() => selectFile(medium['src'])}
             onDoubleClick={() => returnFileUrl(medium['src'])}
           >
             <div class="w3-card">
-              <img className="w3-image w3-round" alt={medium['alt']} src={medium['src']} />
+              <div className="image-wrapper">
+                <img className="w3-image w3-modal-content w3-round" alt={medium['alt']} src={medium['src']} />
+
+              </div>
               <div className="w3-container">
                 <h4>{medium['name']}</h4>
               </div>
@@ -70,6 +125,16 @@ const FileManager = () => {
           </div>
         )
       })}
+    </div>
+    <div className="media-details-toggle-btn" onClick={toggleDetails}>
+      <i className="fa fa-caret-up" />
+      <i className="fa fa-caret-down" />
+    </div>
+    <div className="media-details">
+      <div className="media-details-wrapper">
+      </div>
+      <div className="medium-wrapper w3-col s4">
+      </div>
     </div>
   </div>;
 };
