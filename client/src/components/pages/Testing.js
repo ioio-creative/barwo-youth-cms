e89462c30e1f208903c7357082a4a8b0989369ac
hcc1232001 <qes4d26@hotmail.com>
2020-06-30 16:04:34 +0800

    update
    doing the file explorer

--- client/src/components/pages/Testing.js
+++ client/src/components/pages/Testing.js
@@ -3,6 +3,7 @@ import TitlebarContext from 'contexts/titlebar/titlebarContext';
 import RichTextbox from 'components/form/RichTextbox';
 import FileManager from 'components/form/FileManager';
 import routes from 'globals/routes';
+import { Link, generatePath } from 'react-router-dom';
 
 const Testing = _ => {
   const { setTitle, removeTitle } = useContext(TitlebarContext);
@@ -16,13 +17,21 @@ const Testing = _ => {
     // eslint-disable-next-line
   }, []);
 
-  return <div className='w3-container'>
-    <RichTextbox
-      // debug={true}
-      onChange={(e) => console.log(e)}
-      filebrowserBrowseUrl={routes.fileManager}
-      className={'w3-half'}
-    />
+  return <div className=''>
+    <div className="w3-row">
+      <RichTextbox
+        // debug={true}
+        onChange={(e) => console.log(e)}
+        filebrowserBrowseUrl={routes.fileManager}
+        className={'w3-half'}
+      />
+    </div>
+    <div className="">
+      <button onClick={() => window.open(generatePath(routes.fileManager, { fileType: "images" }))}>open image file manager</button>
+      <button onClick={() => window.open(generatePath(routes.fileManager, { fileType: "videos" }))}>open video file manager</button>
+      <button onClick={() => window.open(generatePath(routes.fileManager, { fileType: "audios" }))}>open audio file manager</button>
+      <button onClick={() => window.open(generatePath(routes.fileManager, { fileType: "pdfs" }))}>open pdf file manager</button>
+    </div>
     {/* <FileManager /> */}
   </div>;
 };
