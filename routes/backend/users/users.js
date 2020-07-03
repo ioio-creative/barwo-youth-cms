8a78d362c35a019989f1087806c30337c915733f
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2020-07-03 20:01:54 +0800

    Update users.js

--- routes/backend/users/users.js
+++ routes/backend/users/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
-const bcrypt = require('bcryptjs');
 const config = require('config');
 
 const auth = require('../../../middleware/auth');
