f5564ab954732aeb1cf23e3786d1e3b691935f81
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2020-07-22 18:24:27 +0800

    removed LANGUAGE_INVALID response type

--- middleware/languageHandling.js
+++ middleware/languageHandling.js
@@ -5,12 +5,6 @@ module.exports = function (req, res, next) {
   const langParam = req.params.lang;
 
   const lang = getLanguageByRouteParam(langParam);
-  if (!lang) {
-    // 400 bad request
-    return res.status(400).json({
-      errors: [LANGUAGE_INVALID]
-    });
-  }
 
   req.language = lang;
   next();
