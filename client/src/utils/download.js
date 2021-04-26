b126c80264153814a8b0d8addcb8dbbaf43a5c98
hcc1232001 <qes4d26@hotmail.com>
2021-04-26 18:13:42 +0800

    Update download.js

--- client/src/utils/download.js
+++ client/src/utils/download.js
@@ -2,5 +2,8 @@ import download from 'downloadjs';
 
 // https://www.npmjs.com/package/downloadjs
 export default (data, strFileName, strMimeType) => {
-  download(data, strFileName, strMimeType);
+  // download(data, strFileName, strMimeType);
+
+
+  download(new Blob(data), strFileName, strMimeType);
 };
