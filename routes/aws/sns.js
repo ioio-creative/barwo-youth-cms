11294f7077d331589b8d3115685eacf6bced3cb8
hcc1232001 <qes4d26@hotmail.com>
2021-04-28 13:15:07 +0800

    Update sns.js

--- routes/aws/sns.js
+++ routes/aws/sns.js
@@ -24,7 +24,7 @@ const topicArnBounce =
 const websiteBackendRoot = config.get('WebsiteBackend.root');
 
 const paramsTopicBounces = {
-  Protocol: 'http',
+  Protocol: 'https',
   TopicArn: topicArnBounce,
   Endpoint: `${websiteBackendRoot}api/aws/sns/handle-bounces`
 };
@@ -33,7 +33,7 @@ const topicArnComplaint =
   'arn:aws:sns:ap-southeast-1:697502200750:ses-complaints-topic-prod';
 
 const paramsTopicComplaints = {
-  Protocol: 'http',
+  Protocol: 'https',
   TopicArn: topicArnComplaint,
   Endpoint: `${websiteBackendRoot}api/aws/sns/handle-complaints`
 };
